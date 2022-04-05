18a1da4a224d5264ef09044b58335049b4f6e2d7
Naresh Shahi <thenraxz@gmail.com>
2022-04-05 07:49:29 +0100

    Test case added and tested successfully

--- spec/Cart/BasketSpec.php
+++ spec/Cart/BasketSpec.php
@@ -25,5 +25,57 @@ class BasketSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(Basket::class);
-    }   
+    }
+
+    function it_calculates_the_total_of_a_scanned_product()
+    {
+        $this->scan('1001');                
+        $this->total(false)->shouldReturn(200.00);
+    }
+    
+    function it_calculates_the_total_of_a_scanned_product_with_discount()
+    {
+        $this->scan('1001');                
+        $this->total(true)->shouldReturn(180.00);
+    }
+
+    function it_calculates_the_total_of_two_scanned_products()
+    {
+        $this->scan('1001');
+        $this->scan('1002');                
+        $this->total(false)->shouldReturn(300.00);
+    }
+
+    function it_calculates_the_total_of_two_scanned_products_with_discount()
+    {
+        $this->scan('1001');
+        $this->scan('1002');                
+        $this->total(true)->shouldReturn(270.00);
+    }
+
+    
+    function it_calculates_the_total_of_three_scanned_products_with_discount()
+    {
+        $this->scan('1001');
+        $this->scan('1002');
+        $this->scan('1003');
+        $this->total(false)->shouldReturn(383.50);
+    }
+    
+    function it_calculates_the_total_of_four_scanned_products()
+    {
+        $this->scan('1001');
+        $this->scan('1002');
+        $this->scan('1003');
+        $this->scan('1004');
+        $this->total(false)->shouldReturn(434.5);
+    }
+    function it_calculates_the_total_of_four_scanned_products_with_discount()
+    {
+        $this->scan('1001');
+        $this->scan('1002');
+        $this->scan('1003');
+        $this->scan('1004');
+        $this->total(true)->shouldReturn(391.05);
+    }
 }
